065e01895cfc0ae96a2fc44914726f70a6062ebf
박수현 <andnan2244@gmail.com>
2022-10-12 15:17:32 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1656 ms, Memory: 69808 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -0,0 +1,11 @@
+import sys
+
+t = int(input())
+
+print_lst = []
+for i in range(t):
+    a, b = list(map(int, sys.stdin.readline().split()))
+    print_lst.append(a + b)
+
+for i in print_lst:
+    print(i)

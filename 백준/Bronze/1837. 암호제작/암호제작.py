86cb633ee940947d9c970368bea7c42816fcf1bf
박수현 <andnan2244@gmail.com>
2022-11-26 22:33:37 +0900

    [Bronze III] Title: 암호제작, Time: 332 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1837. 암호제작/암호제작.py
+++ 백준/Bronze/1837. 암호제작/암호제작.py
@@ -2,8 +2,8 @@ p, k = map(int, input().split())
 t = True
 for i in range(2, k):
     if p % i == 0:
-        print('BAD', i)
+        print("BAD", i)
         t = False
         break
 if t:
-    print('GOOD')
+    print("GOOD")

6b016ff945c7cca0129e4fd7b1cb0caf42e03f2e
박수현 <andnan2244@gmail.com>
2022-12-10 18:09:05 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split()) 
+coin = []
+for i in range(n):
+    coin.append(int(input()))
+
+count = 0
+for i in range(n)[::-1]:
+    count += k // coin[i]
+    k %= coin[i]
+
+print(count)

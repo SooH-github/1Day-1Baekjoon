69008ab6386e6aa9b12c14aac38a1d9dab619bff
박수현 <andnan2244@gmail.com>
2023-04-22 16:46:06 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+c_lst = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+f = c_lst.index(input())
+s = c_lst.index(input())
+t = c_lst.index(input())
+
+print(int(str(f) + str(s)) * (10 ** t))

e31088fd41081acb6d5e14985310a332fccf3ab0
박수현 <andnan2244@gmail.com>
2024-12-16 10:03:18 +0900

    [Silver III] Title: 프린터 큐, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -0,0 +1,22 @@
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, input().split())
+    data = list(map(int, input().split()))
+    
+    result = 1
+    while data:
+        if data[0] < max(data):
+            data.append(data.pop(0))
+        else:
+            if m == 0:
+                break
+            data.pop(0)
+            result += 1
+
+        if m > 0:
+            m = m - 1
+        else:
+            m = len(data) - 1
+
+    print(result)

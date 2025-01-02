58e566e24d284fbb699f4740572448d1587c6eef
박수현 <andnan2244@gmail.com>
2025-01-02 13:49:27 +0900

    [Silver III] Title: Four Squares, Time: 44 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -0,0 +1,18 @@
+import math
+
+def four_squares(n):
+    if int(math.sqrt(n)) ** 2 == n:
+        return 1
+
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if int(math.sqrt(n - i * i)) ** 2 == (n - i * i):
+            return 2
+
+    for i in range(1, int(math.sqrt(n)) + 1):
+        for j in range(1, int(math.sqrt(n - i * i)) + 1):
+            if int(math.sqrt(n - i * i - j * j)) ** 2 == (n - i * i - j * j):
+                return 3
+
+    return 4
+
+print(four_squares(int(input())))
